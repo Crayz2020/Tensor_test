bb25e0ba18fe5112effebb14ecbd0a60a677b73e
Crayz2020 <andrey_zamer@mail.ru>
2022-05-18 19:35:11 +0500

    added locators and data

--- src/test/java/tests/TensorTests.java
+++ src/test/java/tests/TensorTests.java
@@ -1,7 +1,11 @@
 package tests;
 
 import com.codeborne.selenide.Configuration;
+import com.codeborne.selenide.SelenideElement;
+import com.codeborne.selenide.logevents.SelenideLogger;
+import io.qameta.allure.selenide.AllureSelenide;
 import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 
@@ -15,46 +19,78 @@ import static com.codeborne.selenide.Selenide.*;
 
 public class TensorTests {
 
+    @BeforeAll
+    static void beforeAll() {
+        SelenideLogger.addListener("allure", new AllureSelenide());
+
+    }
+    @BeforeEach
+    public void beforeEach() {
+        open("https://tensor.ru/");
+    }
+
+
+
     @Test
     void tensorMainHeaderTest() {
-        open("https://tensor.ru/");
-        $(".tensor_ru-Index__Banner-title").shouldHave(text("Разработка"));
+        headerTitle.shouldHave(text(headerText));
 
     }
 
     @Test
     void tensorAboutTest() {
-        open("https://tensor.ru/");
-        $(byText("О компании")).click();
-        $(".tensor_ru-About__block-title-block").shouldHave(text("Работаем"));
+        $(byText(about)).click();
+        aboutTitle.shouldHave(text(aboutText));
 
     }
 
     @Test
     void tensorBranchesTest() {
-        open("https://tensor.ru/");
-        $(byText("Филиалы")).click();
-        $(".controls-Field").setValue("Уфа").pressEnter();
-        $(".tensor_ru-Branches-list__item-address").shouldHave(text("Октябрьской Революции"));
+        $(byText(branches)).click();
+        cityField.setValue(city).pressEnter();
+        branchesAddress.shouldHave(text(street));
 
     }
 
     @Test
     void tensorSeviceCenterTest() {
-        open("https://tensor.ru/");
-        $(".tensor_ru-Header__button-area").click();
+        moreMenuInformation.click();
         sleep(5000);
-        $(byText("Сервисный центр и ЦТО")).click();
-        $(".tensor_ru-CTO__Banner-btn").click();
-        sleep(40000);
-        $(".ofd-dialog_request-person").$(".controls-TextBox__field").setValue("Андрей");
-        $(".ofd-dialog_request-contacts").$(".controls-FormattedTextBox__field").click();
-        $(".ofd-dialog_request-contacts").$(".controls-FormattedTextBox__field").sendKeys("1234567890");
-        $(".ofd-dialog_request-checkbox-caption").click();
-        $(".controls-Button__text").click();
-        $(".ws-infobox-type-error").shouldBe(visible);
+        $(byText(serviceCenter)).click();
+        submitApplication.click();
+        sleep(20000);
+        setName.setValue(name);
+        setUserNumber.click();
+        setUserNumber.sendKeys(userNumber);
+        privacyAgree.click();
+        textBoxSubmit.click();
+        errorType.shouldBe(visible);
 
     }
+// Locators
+SelenideElement
+        headerTitle = $(".tensor_ru-Index__Banner-title"),
+        aboutTitle = $(".tensor_ru-About__block-title-block"),
+        cityField = $(".controls-Field"),
+        branchesAddress = $(".tensor_ru-Branches-list__item-address"),
+        moreMenuInformation = $(".tensor_ru-Header__button-area"),
+        submitApplication = $(".tensor_ru-CTO__Banner-btn"),
+        setName = $(".ofd-dialog_request-person").$(".controls-TextBox__field"),
+        setUserNumber = $(".ofd-dialog_request-contacts").$(".controls-FormattedTextBox__field"),
+        privacyAgree = $(".ofd-dialog_request-checkbox-caption"),
+        textBoxSubmit = $(".controls-Button__text"),
+        errorType = $(".ws-infobox-type-error");
 
-
+    //Data
+String
+    headerText = "Разработка",
+    about = "О компании",
+    aboutText = "Работаем",
+    branches = "Филиалы",
+    city = "Уфа",
+    street = "Октябрьской Революции",
+    serviceCenter = "Сервисный центр и ЦТО",
+    name = "Андрей",
+    userNumber = "1234567890";
 }
+
